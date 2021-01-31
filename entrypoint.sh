2f14027a216b5971f88ff6d9c61dff34218d8e7f
LYUZhi Pan <lyuzhi.pan@gmail.com>
2021-01-31 11:58:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 npm install -g minify
+apt-get update
+apt-get -y install moreutils
 
 src=$INPUT_SRC
 
 if [ -z "$INPUT_DIST" ]
 then
-    dist=src
+    dist=$src
 else
     dist=$INPUT_DIST
 fi
